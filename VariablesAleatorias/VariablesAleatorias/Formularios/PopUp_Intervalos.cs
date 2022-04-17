d9d6b077433b6bba738e08343ec22fad2ad07c14
CarlosSalinas97 <carlos.ariel.salinas@hotmail.com>
2022-04-16 21:15:11 -0300

    Agregar histograma de frecuencias + popup

---
+++ VariablesAleatorias/VariablesAleatorias/Formularios/PopUp_Intervalos.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VariablesAleatorias.Formularios
+{
+    public partial class PopUp_Intervalos : Form
+    {
+        private int intervalos_seleccionado;
+        public double[] serie_generada;
+
+        public PopUp_Intervalos()
+        {
+            InitializeComponent();
+        }
+
+        private void PopUp_Intervalos_Load(object sender, EventArgs e)
+        {
+            cmb_intervalos.Items.Add(5);
+            cmb_intervalos.Items.Add(10);
+            cmb_intervalos.Items.Add(15);
+            cmb_intervalos.Items.Add(20);
+            btn_Continuar.Enabled = false;
+        }
+
+        private void btn_Volver_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btn_Continuar_Click(object sender, EventArgs e)
+        {
+            Histograma formHistograma = new Histograma();
+            formHistograma.intervalos_seleccionado = intervalos_seleccionado;
+            formHistograma.serie_generada = serie_generada;
+            formHistograma.Show();
+            Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            intervalos_seleccionado = (int)cmb_intervalos.SelectedItem;
+            btn_Continuar.Enabled = true;
+        }
+    }
+}
